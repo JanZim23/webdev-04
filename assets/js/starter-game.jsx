8ebd4ca8476e10f7065e3742ca04ff408ba3d68a
Jan Zimmermann <janzim23@gmail.com>
2018-09-28 17:23:31 -0400

    First

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -1,24 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
+import Tile from './tile'
 
 export default function game_init(root) {
   ReactDOM.render(<Starter />, root);
 }
 
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+const getTiles = letters.reduce((a, letter) => {
+  a.push(createTile(letter), createTile(letter));
+});
+
+
+const initialState = () =>
+  Object.assign({},
+    {
+      locked: false,
+      tiles: getTiles().sort(() => 0.5 - Math.random() * Math.random()),
+      first: false,
+      second: false;
+    });
+
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = initialState();
+
   }
 
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
+  tileClicked(tile) {
+    if(this.state.locked) {
+
+      //Fishy.
+      this.state.first.deselect();
+      this.state.second.deselect();
+      //End Fishy
+      unlock();
+    }
+
+    if(this.state.first == false) {
+      this.setState({first: tile});
+      tile.select();
+    } elseif(this.state.second == false) {
+      this.setState({second: tile});
+      checkFirstSecond();
+    }
   }
 
-  hax(_ev) {
-    alert("hax!");
+  checkFirstSecond() {
+    if(this.state.first.props.letter == this.state.second.props.letter) {
+      this.state.first.complete();
+      this.state.second.complete();
+    } else {
+      lock();
+    }
+  }
+
+  lock() {
+    this.setState({locked: true});
+    //Wait a second and call unlock.
+  }
+  unlock() {
+    this.setState({first: false, second: false});
+    this.setState({locked: false});
   }
 
   render() {
@@ -44,4 +90,3 @@ class Starter extends React.Component {
     }
   }
 }
-
---
+++ assets/js/tile.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+
+const createTile = (letter) => {
+  return (<Tile
+            letter={letter}
+            label={letter}
+            selected={false}
+            complete={false}
+            onclick={false}
+            />);
+}
+
+class Tile extends react.Component {
+  constructor(props) {
+    props.onclick = () => {Starter.tileClicked(this);};
+    super(props)
+    this.state= {selected, complete};
+  }
+
+  select() {
+    this.setState({selected: true});
+  }
+
+  deselect() {
+    this.setState({selected: false});
+  }
+
+  complete() {
+    this.setState({complete: true});
+  }
+
+
+  render() {
+    if(this.state.complete) {
+      return (<button disabled>{this.props.label}</button>);
+    } else if (selected) {
+      return (<button disabled>{this.props.label}</button>);
+    } else {
+      return (<button onClick={this.props.onclick}>?</button>);
+    }
+  }
+}
