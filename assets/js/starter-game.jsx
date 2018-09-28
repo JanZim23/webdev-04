0ca86deb25b8a0138db7998386717f552a82ddaa
Jan Zimmermann <janzim23@gmail.com>
2018-09-28 17:31:27 -0400

    Second

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -68,25 +68,6 @@ class Starter extends React.Component {
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
-    }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
+    return this.state.tiles.join();
   }
 }
